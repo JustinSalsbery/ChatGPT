019689d44dee41c237aacccb17f22aed70bd050d
JustinSalsbery <justin.salsbery14@gmail.com>
2025-04-10 17:56:27 -0700

    feat: Added border option and updated models.

--- chat.py
+++ chat.py
@@ -8,19 +8,21 @@ import argparse
 
 from openai import OpenAI  # requires install
 
+
 # constants
 FILE_PATH = os.path.expanduser("~/.chat")
-MODELS = ["gpt-3.5-turbo", "gpt-4"]
-VERSION = "2.2"
+MODELS = {"3.5": "gpt-3.5-turbo", "4o": "gpt-4o", "o3": "o3-mini"}
+VERSION = "2.3"
 
 # defaults
-model = 3
-retain = 3
+model = "gpt-3.5-turbo"
 temperature = 1.0
 instructions = ("If you are asked to complete a complex task, break the task into "
                 "multiple steps and reason your way through each step. Otherwise, "
                 "keep responses short and direct.")
+retain = 3
 messages = []
+border = 1
 
 
 # simplifies the default help format
@@ -35,22 +37,26 @@ class CustomHelpFormatter(argparse.HelpFormatter):
 
 
 # set up argument parser
-parser = argparse.ArgumentParser(
-    description="terminal ChatGPT interface", formatter_class=CustomHelpFormatter)
+parser = argparse.ArgumentParser(description="terminal ChatGPT interface", 
+                                 formatter_class=CustomHelpFormatter)
+
 parser.add_argument("prompt", nargs="?",
                     help='text prompt, e.g., "Explain how pi is calculated."')
-parser.add_argument("-v", "--version", action="version",
-                    version=f'%(prog)s {VERSION}', help="show version number and exit")
 parser.add_argument("-s", "--settings", action="store_true",
                     help="show current settings and exit")
-parser.add_argument("-m", "--model", type=int,
-                    choices=[3, 4], help="[3, 4] model selected")
-parser.add_argument("-r", "--retain", type=int, choices=range(0, 10),
-                    help="[0, 9] messages retained in conversation")
-parser.add_argument("-t", "--temperature", type=float,
-                    choices=[x / 10.0 for x in range(1, 20)], help="[0.1, 1.9] creativity")
+parser.add_argument("-m", "--model", type=str, choices=["3.5", "4o", "o3"], 
+                    help="select model {3.5, 4o, o3}")
+parser.add_argument("-t", "--temperature", type=float, choices=[x / 10.0 for x in range(1, 20)], 
+                    help="[0.1, 1.9] creativity, with 1.9 being the most creative")
 parser.add_argument("-i", "--instructions", type=str,
                     help="instructions for model")
+parser.add_argument("-r", "--retain", type=int, choices=range(0, 10),
+                    help="retain [0, 9] messages in the conversation")
+parser.add_argument("-b", "--border", type=int, choices=[0, 1],
+                    help="output borders around the response {0, 1}")
+parser.add_argument("-v", "--version", action="version", version=f'%(prog)s {VERSION}', 
+                    help="show version number and exit")
+
 
 # parse command line arguments
 args = parser.parse_args()
@@ -81,18 +87,21 @@ def check_messages(messages):
 try:
     with open(FILE_PATH, "r") as file:
         memory = json.load(file)
-        model = int(memory["model"])
-        retain = int(memory["retain"])
+
+        model = memory["model"]
         temperature = float(memory["temperature"])
         instructions = memory["instructions"]
+        retain = int(memory["retain"])
         messages = check_messages(memory["messages"])
+        border = int(memory["border"])
 except Exception as e:
     pass  # continue using defaults
+
 
 # update defaults with arguments
 if args.model is not None:
     model = args.model
-model = model if 3 <= model <= 4 else 3
+model = model if model in MODELS else "3.5"
 
 if args.retain is not None:
     retain = args.retain
@@ -105,14 +114,20 @@ temperature = temperature if 0.0 < temperature < 2.0 else 1.0
 if args.instructions is not None:
     instructions = args.instructions
 
+if args.border is not None:
+    border = args.border
+
+
 # print settings and exit
 if args.settings:
     print("settings:")
-    print(f"  model: {MODELS[model - 3]}")
-    print(f"  retain: {retain}")
+    print(f"  model: {model}")
     print(f"  temperature: {temperature}")
     print(f'  instructions: "{instructions}"')
+    print(f"  retain: {retain}")
+    print(f"  border: {border}")
     sys.exit(0)
+
 
 # make API call
 if args.prompt is not None:
@@ -123,7 +138,7 @@ if args.prompt is not None:
 
     openai = OpenAI()
     completion = openai.chat.completions.create(
-        model=MODELS[model - 3],
+        model=MODELS[model],
         temperature=temperature,
         messages=messages
     ).choices[0].message.content
@@ -135,19 +150,25 @@ if args.prompt is not None:
 
     terminal_width = shutil.get_terminal_size().columns
 
-    print("*" * terminal_width)
-    print(completion)
-    print("*" * terminal_width)
+    if border:
+        print("*" * terminal_width)
 
-# rewrite settings file
+    print(completion)
+
+    if border:
+        print("*" * terminal_width)
+
+
+# write settings file
 with open(FILE_PATH, "w") as file:
     messages = [] if retain == 0 else messages[retain * -2:]
 
     memory = {
         "model": model,
-        "retain": retain,
         "temperature": temperature,
         "instructions": instructions,
+        "retain": retain,
+        "border": border,
         "messages": messages
     }
 
